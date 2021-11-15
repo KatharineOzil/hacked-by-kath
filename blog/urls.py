5165745d44ed19b16ed51a74bacb5ee984b46a96
KatharineOzil <katharineozil@gmail.com>
2021-11-15 16:38:11 +0800

    fix url bug

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-   #path('', views.index, name='index'),
-   #path(r'archive', views.archive, name='archive'),
    path('', views.archive, name='index'),
+   path(r'archive', views.archive, name='archive'),
    path(r'article/(?P<id>\d+)/$', views.detail, name='detail'),
    path(r'aboutme', views.aboutme, name='aboutme'),
    path(r'404', views.page_not_found, name='404')
